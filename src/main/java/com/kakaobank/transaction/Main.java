55d87496e941361365f10614ff8678ae440975b3
jypark-alticast <jy.park@alticast.com>
2023-03-19 15:54:49 +0900

    Main 파일 추가.

---
+++ src/main/java/com/kakaobank/transaction/Main.java
@@ -0,0 +1,15 @@
+package com.kakaobank.transaction;
+
+import com.kakaobank.transaction.exception.TransactionGeneratorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) throws TransactionGeneratorException {
+        logger.info("TransactionGenerator start.");
+
+        logger.info("TransactionGenerator start.");
+    }
+}
--- src/main/java/com/kakaocorp/transaction/Main.java
+++
@@ -1,7 +0,0 @@
-package com.kakaocorp.transaction;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
